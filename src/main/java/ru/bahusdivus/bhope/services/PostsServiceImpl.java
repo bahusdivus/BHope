2244b163b7bc7b2eaeb0a67756dd97528b9a8739
Tatanka Naumovets <49755327+tatanka27@users.noreply.github.com>
2019-07-24 17:18:02 +0300

    Сортировка (#10)
    * Добавлена регистрация, идентификация и аутентификация
    
    * Добавлен префикс у названий таблиц, захешированы пароли у "начальных" юзеров
    
    * Поиск поста по имени.
    
    * Счетчик лайков.
    
    *Вывод самых популярных постов на главной.
    
    *Header -> Title
    
    * Поиск по имени без учета регистра.
    
    * Сортировка по дате и по лайкам.

--- src/main/java/ru/bahusdivus/bhope/services/PostsServiceImpl.java
+++ src/main/java/ru/bahusdivus/bhope/services/PostsServiceImpl.java
@@ -54,27 +54,34 @@ public class PostsServiceImpl implements PostsService {
     }
 
     @Override
-    public List<PostDto> getPosts() {
-        List<Post> posts = postRepository.findAll();
+    public List<PostDto> getPostsOrderByDate() {
+        List<Post> posts = postRepository.findByDeletedFalseOrderByDateDesc();
         return posts.stream()
                 .map(PostDto::new)
-                .sorted(Comparator.comparing(PostDto::getDate).reversed())
                 .collect(Collectors.toList());
     }
 
     @Override
-    public List<PostDto> getPostByLike() {
-        List<Post> posts = postRepository.findAll();
+    public List<PostDto> getPostsOrderByLikeCount() {
+        List<Post> posts = postRepository.findByDeletedFalseOrderByLikeCountDesc();
         return posts.stream()
                 .map(PostDto::new)
-                .sorted(Comparator.comparing(PostDto::getLikeCount).thenComparing(PostDto::getDate).reversed())
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<PostDto> getPostsByLike() {
+        List<Post> posts = postRepository.findByDeletedFalse();
+        return posts.stream()
+                .map(PostDto::new)
+                .sorted(Comparator.comparing(PostDto::getDate).thenComparing(PostDto::getLikeCount).reversed())
                 .limit(10)
                 .collect(Collectors.toList());
     }
 
     @Override
     public List<PostDto> getPostsByUserId(long userId) {
-        List<Post> posts = postRepository.findByUserId(userId);
+        List<Post> posts = postRepository.findByUserIdAndDeletedFalse(userId);
         return posts.stream()
                 .map(PostDto::new)
                 .sorted(Comparator.comparing(PostDto::getDate).reversed())
@@ -83,7 +90,7 @@ public class PostsServiceImpl implements PostsService {
 
     @Override
      public List<PostDto> getPostsByUserName(String userName) {
-        List<Post> posts = postRepository.findByUserName(userName);
+        List<Post> posts = postRepository.findByUserName(userName.toLowerCase());
         return posts.stream()
                 .map(PostDto::new)
                 .sorted(Comparator.comparing(PostDto::getDate).reversed())
